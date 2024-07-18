d9471b55f2e8621478e9703d940adb16b2ce5537
Mubeen <mubeenshaikh8072@gmail.com>
2024-07-18 04:54:13 -0400

    changed port

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ dotenv.config({ path: './variables.env' });
 var http = require('http');
 var app = require('./server')
 var server = http.createServer(app)
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 80
 server.listen(PORT, console.log(`app is running on port ${PORT}`));
